25199fccb5f05307e51a31c8ad67d90f52ca0943
Patrick O'Brien <obrienpja@gmail.com>
2017-11-24 19:39:20 -0500

    Saves data to file

--- mcmc.cpp
+++ mcmc.cpp
@@ -1,9 +1,19 @@
 // normal_distribution
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <iostream>
-#include <string>
-#include <random>
+#include <vector>
+#include <numeric>
+#include <iterator>
 #include <functional>
+#include <string>
 #include <algorithm>
+#include <fstream>
+#include <thread>
+
+#include "../matplotlib-cpp/matplotlibcpp.h"
+namespace plt = matplotlibcpp;
 
 const double pi = 3.1415926535897;
 
@@ -53,6 +63,14 @@ double prod(std::vector<double> vec)
 // }
 
 
+void saveData(std::string filename, std::vector<double> & data)
+{
+  std::ofstream output_file(filename);
+  std::ostream_iterator<double> output_iterator(output_file, "\n");
+  std::copy(data.begin(), data.end(), output_iterator);
+}
+
+
 std::vector<double> sampler(std::vector<double> dats, int samples, double mu_init, double proposal_width, double mu_prior_mu, double mu_prior_sd)
 {
   double mu_current = mu_init;
@@ -92,6 +110,30 @@ std::vector<double> sampler(std::vector<double> dats, int samples, double mu_ini
 }
 
 
+void plotCurves(std::vector<double> equity, std::string filename)
+{
+// Prepare data.
+int n = equity.size();
+std::vector<double> x(n);
+for(int i=0; i<n; ++i)
+  x.at(i) = i;
+
+// Plot line from given x and y data. Color is selected automatically.
+plt::plot(x, equity, "bo");
+
+// Set x-axis to interval [0,1000000]
+plt::xlim(0, n);
+
+// Add graph title
+plt::title("Equity Curve");
+// Enable legend.
+plt::legend();
+// save figure
+plt::save(filename);
+// plt::show();
+}
+
+
 int main()
 {
   srand (time(NULL));
@@ -105,6 +147,8 @@ int main()
   }
 
   std::vector<double> post = sampler(data, 1000000, -1., .5, 0, 1);
+
+  saveData("Data/result.txt", post);
 
   return 0;
 }
